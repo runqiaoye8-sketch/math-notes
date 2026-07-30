a69c93fc3fd3db8c3d3a1935d1fe97bbff552222
runqiaoye8-sketch <runqiaoye8@gmail.com>
2026-07-30 17:33:48 +0800

    Update detail.js

--- math-notes/js/detail.js
+++ math-notes/js/detail.js
@@ -1,22 +1,17 @@
-let id=
+let id =
 new URLSearchParams(location.search)
 .get("id");
 
 
-
 fetch("data/questions.json")
-
 
 .then(res=>res.json())
 
-
 .then(data=>{
-
 
 let q=data.find(
 item=>item.id==id
 );
-
 
 
 document.title=q.标题;
@@ -26,30 +21,24 @@ document.getElementById("title")
 .innerHTML=q.标题;
 
 
-
 document.getElementById("question")
 .innerHTML=q.题目;
 
 
-renderMath();
-
-
-
 document.getElementById("hard")
-.innerHTML=q.难点;
-
+.innerHTML=q.难点.replace(/\n/g,"<br>");
 
 
 document.getElementById("answer")
-.innerHTML=
-q.答案.replace(/\n/g,"<br>");
-
+.innerHTML=q.答案.replace(/\n/g,"<br>");
 
 
 document.getElementById("analysis")
-.innerHTML=
-q.思路.replace(/\n/g,"<br>");
+.innerHTML=q.思路.replace(/\n/g,"<br>");
 
+
+// 所有内容加载完成后，再渲染公式
+renderMath();
 
 
 });
